03252f6c96a4cbebd350ad99e17142ed7e269b63
Valco <80554644+RiseValco@users.noreply.github.com>
2023-01-21 00:38:09 -0500

    Update and rename hello.js to hwid.js

---
+++ pages/api/hwid.js
@@ -0,0 +1,16 @@
+export default function hwid(req, res) {
+  let hwid = []
+  let detections = ["hwid", "identifier", "fingerprint"]
+  for (let i=0;i<Object.keys(req.headers).length;i++) {
+    console.log(req.headers)
+    let headerName = Object.keys(req.headers)[i]
+    let headerValue = req.headers[headerName]
+    for (let detection of detections) {
+      if (headerName.toLowerCase().includes(detection)) {
+        hwid.push(headerValue)
+      }
+    }
+  
+  }
+  res.status(200).json({ detected: hwid || "none detected" })
+}
